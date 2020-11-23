ebe8b6f458f1b3072ae3f75e1dcebf0fb3725a8b
Serguei Cambour <s.cambour@gmail.com>
2020-11-23 13:30:08 +0100

    Add Cart model tests

--- test/models/cart_test.rb
+++ test/models/cart_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class CartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test '#add_product adds a unique product' do
+    product = products(:ruby)
+    cart = Cart.new
+    line_item = cart.add_product(product)
+    assert_equal 1, cart.line_items.size
+
+    assert_equal line_item.price, product.price
+  end
+
+  test '#add_product adds a duplicate product' do
+    cart = Cart.new
+    cart.add_product(products(:ruby))
+    cart.add_product(products(:ruby))
+
+    assert_equal 2, cart.line_items.size
+  end
 end
