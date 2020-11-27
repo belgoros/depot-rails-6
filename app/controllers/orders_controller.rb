eb745df56e21da5d9d405e3a19d7d6cd52dd0d6a
Serguei Cambour <s.cambour@gmail.com>
2020-11-27 08:50:27 +0100

    Remove empty lines

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -81,7 +81,6 @@ class OrdersController < ApplicationController
     def order_params
       params.require(:order).permit(:name, :address, :email, :pay_type)
     end
-  #...
 
   private
      def ensure_cart_isnt_empty
@@ -89,7 +88,6 @@ class OrdersController < ApplicationController
          redirect_to store_index_url, notice: 'Your cart is empty'
        end
      end
-
 
     def pay_type_params
       if order_params[:pay_type] == "Credit card"
@@ -102,5 +100,4 @@ class OrdersController < ApplicationController
         {}
       end
     end
-
 end
