b6e43b87d1c414edc76f113c07619e6bd1fb6899
Serguei Cambour <serguei.cambour@sodexo.com>
2021-08-11 16:04:21 +0200

    Implement SupportMailbox tests

--- test/mailboxes/support_mailbox_test.rb
+++ test/mailboxes/support_mailbox_test.rb
@@ -1,11 +1,18 @@
 require "test_helper"
 
 class SupportMailboxTest < ActionMailbox::TestCase
-  # test "receive mail" do
-  #   receive_inbound_email_from_mail \
-  #     to: '"someone" <someone@example.com>',
-  #     from: '"else" <else@example.com>',
-  #     subject: "Hello world!",
-  #     body: "Hello?"
-  # end
+  test 'we create a SupportRequest when we get a support email' do
+    receive_inbound_email_from_mail(
+      from: 'chris@somewhere.com',
+      to: 'support@example.com',
+      subject: 'Need help',
+      body: 'I cannot figure out how to check out!!'
+    )
+
+    support_request = SupportRequest.last
+    assert_equal 'chris@somewhere.com', support_request.email
+    assert_equal 'Need help', support_request.subject
+    assert_equal 'I cannot figure out how to check out!!', support_request.body
+    assert_nil support_request.order
+  end
 end
